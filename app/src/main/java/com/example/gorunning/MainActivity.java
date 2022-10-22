c51f636c4a8241c8a633e4a69ee2536e27300614
Liu Chengjie <liuchengjie@lcj.local>
2022-10-22 14:02:12 +0800

    Initial commit

---
+++ app/src/main/java/com/example/gorunning/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.gorunning;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
